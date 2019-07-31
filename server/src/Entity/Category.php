af1d414f3aba116c8ac24725c7210144acbd0477
Maikelb <maikel.bolderdijk@student.hu.nl>
2019-07-31 20:13:14 +0200

    Add category validation on PUT and POST calls

--- server/src/Entity/Category.php
+++ server/src/Entity/Category.php
@@ -20,11 +20,21 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=42)
+     * @Assert\NotNull
+     * @Assert\Length(
+     *     min = 2,
+     *     max = 42,
+     *     groups={"postValidation", "putValidation"} 
+     * )
+
      */
     private $title;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Assert\Length(
+     *     min = 2,
+     *     groups={"postValidation", "putValidation"} 
      */
     private $details;
 
